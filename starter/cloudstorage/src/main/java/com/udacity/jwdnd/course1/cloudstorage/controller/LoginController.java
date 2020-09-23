c3bc39014f79520d00034ca7665874bab11538e6
keithlockey <lockey.keith@gmail.com>
2020-09-23 13:30:30 -0400

    redirection stuff

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,20 +1,23 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+//@RequestMapping("/login")
 public class LoginController {
 
-//    @GetMapping()
-//    public String loginView() {
-//        System.out.println("loginview");
-//        return "login";
+        @GetMapping("/login")
+        public String loginView() {
+            System.out.println("loginview");
+            return "login";
+        }
 
     @RequestMapping("/")
     public String redirect() {
+        System.out.println("redirect");
         return "redirect:/login?param=error";
     }
 
